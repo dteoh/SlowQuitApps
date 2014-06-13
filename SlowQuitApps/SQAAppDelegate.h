810d34e27e18407cb76d52882a6dcf47555288aa
Douglas Teoh <douglas@dteoh.com>
2014-06-13 10:59:03 +1000

    Listen to Cmd-Q

--- SlowQuitApps/SQAAppDelegate.h
+++ SlowQuitApps/SQAAppDelegate.h
@@ -1,12 +1,4 @@
-//
-//  SQAAppDelegate.h
-//  SlowQuitApps
-//
-//  Created by Douglas Teoh on 13/06/14.
-//  Copyright (c) 2014 Douglas Teoh. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
+@import Cocoa;
 
 @interface SQAAppDelegate : NSObject <NSApplicationDelegate>
 
--- SlowQuitApps/SQAAppDelegate.m
+++ SlowQuitApps/SQAAppDelegate.m
@@ -1,18 +1,27 @@
-//
-//  SQAAppDelegate.m
-//  SlowQuitApps
-//
-//  Created by Douglas Teoh on 13/06/14.
-//  Copyright (c) 2014 Douglas Teoh. All rights reserved.
-//
-
+@import Carbon;
 #import "SQAAppDelegate.h"
 
 @implementation SQAAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    EventHotKeyRef hotKeyRef;
+    EventHotKeyID hotKeyID;
+    EventTypeSpec eventType;
+    eventType.eventClass = kEventClassKeyboard;
+    eventType.eventKind = kEventHotKeyPressed;
+
+    InstallApplicationEventHandler(&cmdQHandler, 1, &eventType, NULL, NULL);
+    hotKeyID.signature = 'sqad';
+    hotKeyID.id = 1;
+
+    RegisterEventHotKey(kVK_ANSI_Q, cmdKey, hotKeyID, GetApplicationEventTarget(),
+                        kEventHotKeyExclusive, &hotKeyRef);
+}
+
+OSStatus cmdQHandler(EventHandlerCallRef nextHandler, EventRef anEvent, void *userData) {
+    NSLog(@"Cmd-Q pressed");
+    return noErr;
 }
 
 @end
