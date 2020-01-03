684572b1a90dcd73c4983708051ac46eb35114fa
Douglas Teoh <douglas@dteoh.com>
2020-01-03 10:31:11 +0900

    Display name of app about to be terminated

--- SlowQuitApps/SQAOverlayWindowController.m
+++ SlowQuitApps/SQAOverlayWindowController.m
@@ -5,6 +5,7 @@
 @interface SQAOverlayWindowController () {
 @private
     SQAOverlayView *overlayView;
+    NSTextField *titleView;
 }
 @end
 
@@ -13,17 +14,52 @@
 - (id)init {
     self = [super initWithWindowNibName:@"SQAOverlayWindow"];
     if (self) {
-        NSRect contentRect = NSMakeRect(0, 0, 300, 300);
-        NSPanel *panel = [[NSPanel alloc] initWithContentRect:contentRect
+        // TODO refactor this.
+        // 240 = 200 (actual width of bar) + 20 (padding) + (20 padding)
+        // 20 is defined in the internals of SQAOverlayView.
+        const NSRect overlayFrame = NSMakeRect(0, 0, 240, 240);
+        overlayView = [[SQAOverlayView alloc] initWithFrame:overlayFrame];
+
+        titleView = [NSTextField labelWithString:@"ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+        titleView.editable = NO;
+        titleView.alignment = NSTextAlignmentCenter;
+        titleView.font = [NSFont labelFontOfSize:18];
+        titleView.textColor = NSColor.labelColor;
+        [titleView sizeToFit];
+
+        NSVisualEffectView *titleFxView = [[NSVisualEffectView alloc] init];
+        titleFxView.wantsLayer = YES;
+        titleFxView.layer.masksToBounds = YES;
+        titleFxView.layer.cornerRadius = 2;
+        titleFxView.blendingMode = NSVisualEffectBlendingModeBehindWindow;
+        titleFxView.material = NSVisualEffectMaterialSheet;
+        titleFxView.state = NSVisualEffectStateActive;
+
+        [titleFxView addSubview:titleView];
+
+        NSStackView *stackView = [NSStackView stackViewWithViews:@[overlayView, titleFxView]];
+        stackView.orientation = NSLayoutConstraintOrientationVertical;
+        stackView.alignment = NSLayoutAttributeCenterX;
+        stackView.spacing = 0;
+
+        [NSLayoutConstraint activateConstraints:@[
+            [overlayView.widthAnchor constraintEqualToConstant:overlayFrame.size.width],
+            [overlayView.heightAnchor constraintEqualToConstant:overlayFrame.size.height],
+
+            // Add some horizontal padding to the text field.
+            [titleFxView.widthAnchor constraintEqualToAnchor:titleView.widthAnchor constant:5],
+            // Not padding vertically because there is no easy way to have vertically centered text...
+            [titleFxView.heightAnchor constraintEqualToAnchor:titleView.heightAnchor],
+        ]];
+
+        NSPanel *panel = [[NSPanel alloc] initWithContentRect:stackView.frame
                                                     styleMask:NSWindowStyleMaskBorderless|NSWindowStyleMaskNonactivatingPanel
                                                       backing:NSBackingStoreBuffered
                                                         defer:YES];
-        [panel setOpaque:NO];
+        panel.opaque = NO;
         panel.backgroundColor = NSColor.clearColor;
         panel.level = NSScreenSaverWindowLevel;
-
-        overlayView = [[SQAOverlayView alloc] initWithFrame:contentRect];
-        panel.contentView = overlayView;
+        panel.contentView = stackView;
 
         self.window = panel;
     }
@@ -32,7 +68,10 @@
 
 #pragma mark - SQAOverlayViewInterface implementation
 
-- (void)showOverlay:(CGFloat)duration {
+- (void)showOverlay:(CGFloat)duration withTitle:(NSString * _Nonnull)title {
+    titleView.stringValue = title;
+    [titleView sizeToFit];
+
     [self showWindow:nil];
     [self.window center];
     [self.window makeKeyAndOrderFront:self];
